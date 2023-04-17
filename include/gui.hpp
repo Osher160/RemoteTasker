6ed79b9e79915d1bf3a873ee1817b9f2c3482bef
osher160 <osher160@gmail.com>
2023-04-17 23:16:59 +0300

    stub

---
+++ include/gui.hpp
@@ -0,0 +1,22 @@
+/******************************************************************************/
+/*	Project:	Remote Tasker   										      */
+/*	File:		gui.hpp	    										          */
+/*	Version: 	0.02														  */
+/******************************************************************************/
+#ifndef __GUI_HPP__
+#define __GUI_HPP__
+
+#include <iostream>
+#include <vector>
+
+#include <gtk/gtk.h>
+
+class window
+{
+ public:
+    
+ private:
+
+};
+
+#endif // __GUI_HPP__
